6e482d9966b04da6c6d02e5821b13ea51ca03453
Pete Dring <pddring@googlemail.com>
2013-10-23 11:41:55 +0100

    Added error handling

--- python-scratch/scratch-interface.py
+++ python-scratch/scratch-interface.py
@@ -11,35 +11,42 @@ import scratch
 r = Launcher()
 
 # connect to scratch
-s = scratch.Scratch()
+try:
+	s = scratch.Scratch()
+except:
+	print("Error: You must open scratch before running this program")
+	exit()
 time = 200
 
 print("Connected to USB Missile launcher... waiting for messages from Scratch")
 print("Press Ctrl+C to quit")
 # Loop continuously to wait for messages from scratch
-while True:
-    # get a message broadcast from scratch
-	msg = s.receive()
-	cmd = msg[1]
-    
-    # process the message
-	"""
-	This code will process the following broadcast messages from scratch:
-	MOVE_LEFT: aims the missile launcher slightly to the left
-	MOVE_RIGHT: aims the missile launcher slightly to the right
-	MOVE_UP: hopefully you've got the idea by now...
-	"""
-	if cmd == "MOVE_LEFT":
-		r.move(r.MOVE_LEFT, time)
-	elif cmd == "MOVE_RIGHT":
-		r.move(r.MOVE_RIGHT, time)
-	elif cmd == "MOVE_UP":
-		r.move(r.MOVE_UP, time)
-	elif cmd == "MOVE_DOWN":
-		r.move(r.MOVE_DOWN, time)
-	elif cmd == "FIRE":
-		r.fire()
-	elif cmd == "LED_ON":
-		r.set_led(True)
-	elif cmd == "LED_OFF":
-		r.set_led(False)
+try:
+	while True:
+		# get a message broadcast from scratch
+		msg = s.receive()
+		cmd = msg[1]
+		
+		# process the message
+		"""
+		This code will process the following broadcast messages from scratch:
+		MOVE_LEFT: aims the missile launcher slightly to the left
+		MOVE_RIGHT: aims the missile launcher slightly to the right
+		MOVE_UP: hopefully you've got the idea by now...
+		"""
+		if cmd == "MOVE_LEFT":
+			r.move(r.MOVE_LEFT, time)
+		elif cmd == "MOVE_RIGHT":
+			r.move(r.MOVE_RIGHT, time)
+		elif cmd == "MOVE_UP":
+			r.move(r.MOVE_UP, time)
+		elif cmd == "MOVE_DOWN":
+			r.move(r.MOVE_DOWN, time)
+		elif cmd == "FIRE":
+			r.fire()
+		elif cmd == "LED_ON":
+			r.set_led(True)
+		elif cmd == "LED_OFF":
+			r.set_led(False)
+except:
+	print("Something went wrong - did you disconnect the missile launcher?")
